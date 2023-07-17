dcbbebe95799d4bcae90c6102a1e313ed3b0b685
Artem Ushenko <kingart030192kingart@gmail.com>
2023-07-17 18:04:50 +0300

    Was added documentation for class and every function with description of all arguments and returns

--- password_generator_oop.py
+++ password_generator_oop.py
@@ -3,11 +3,35 @@ import string
 
 
 class PasswordGenerator:
-    """Was created class for object Password Generator"""
+    """
+    Was created class for object Password Generator based on task.
+
+    This class can generate a random password of a specified length, including
+    optional combinations of uppercase and lowercase letters, digits, and special characters.
+
+   Attributes:
+        length (int): Length of the password to be generated.
+        include_uppercase (bool): Whether to include uppercase letters in the password.
+        include_lowercase (bool): Whether to include lowercase letters in the password.
+        include_digits (bool): Whether to include digits in the password.
+        include_special_chars (bool): Whether to include special characters in the password.
+    """
 
     def __init__(self, length=8, include_uppercase=True, include_lowercase=True, include_digits=True,
                  include_special_chars=True):
-        """Method init with some conditions for object Password Generator"""
+        """
+        Method initialization with conditions: length, include uppercase letters, include lowercase letters,
+        include digits and include special symbols for object Password Generator
+
+        Initialize PasswordGenerator with the user's preferences.
+
+        Args:
+        length (int, optional): Desired length of the password. Defaults to 8.
+        include_uppercase (bool, optional): If True, includes uppercase letters in the password. Defaults to True.
+        include_lowercase (bool, optional): If True, includes lowercase letters in the password. Defaults to True.
+        include_digits (bool, optional): If True, includes digits in the password. Defaults to True.
+        include_special_chars (bool, optional): If True, includes special characters in the password. Defaults to True.
+        """
 
         self.length = length
         self.include_uppercase = include_uppercase
@@ -16,7 +40,16 @@ class PasswordGenerator:
         self.include_special_chars = include_special_chars
 
     def generator(self):
-        """The different character sets from which to generate the password"""
+        """
+        The different character sets from which to generate the password
+        Generate and return a random password based on the initialized attributes.
+
+        The generated password can include a mix of uppercase and lowercase letters,
+        digits, and special characters, based on the user's preferences set during initialization.
+
+        Returns:
+        The randomly generated password in type str.
+        """
 
         uppercase_letters = string.ascii_uppercase if self.include_uppercase else ''
         lowercase_letters = string.ascii_lowercase if self.include_lowercase else ''
@@ -33,7 +66,19 @@ class PasswordGenerator:
 
 
 def get_input_parameter(prompt):
-    """Check function for input parameter which must be only yes or no"""
+    """
+    Check function for input parameter which must be only yes or no.
+
+    If the user enters an invalid input (not 'yes' or 'no'), the function will
+    repeatedly ask until a valid input is given.
+
+    Args:
+    'prompt' (str): The question to ask the user.
+
+    Returns:
+    bool: Returns True if user's response is 'yes', False if 'no'.
+
+    """
 
     response = input(prompt)
     if response.lower() in ['yes', 'no']:
@@ -52,10 +97,14 @@ while True:
     except ValueError:
         print("Invalid input. Please enter an integer number.")
 
+# Waiting for the input of responses from the user
 include_uppercase = get_input_parameter("Include uppercase letters (yes/no): ")
 include_lowercase = get_input_parameter("Include lowercase letters (yes/no): ")
 include_digits = get_input_parameter("Include digits (yes/no): ")
 include_special_chars = get_input_parameter("Include special symbols (yes/no): ")
 
+# Declare a variable and assign our class PasswordGenerator to it
 your_password = PasswordGenerator(length, include_uppercase, include_lowercase, include_digits, include_special_chars)
+
+# Print the value to the CLI
 print(f"Your password: {your_password.generator()}")
